2084a4bb9f6410f3f7918e8ecd2dc2ecaff2680c
Shoaib <shoaib.maks@live.com>
2015-04-09 22:34:34 -0700

    Updated models.py to include the Base model

--- learning_journal/learning_journal/models.py
+++ learning_journal/learning_journal/models.py
@@ -25,3 +25,22 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key = True);
+    title = Column(unique=True, Unicode(255), nullable = False)
+    body = Column(UnicodeText, default = 'u')
+    created = Column(DateTime, nullable = False, default = datetime.datetime.utcnow())
+    edited = Column(DateTime, nullable = False, default = datetime.datetime.now(), onupdate=datetime.datetime.utcnow())
+
+    @classmethod
+    def all(cls, session = None):
+        for row in Session.query(Entry).order_by(Entry.edited)
+            print(row.title, row.body, row.created, row.edited)
+
+    @classmethod
+    def by_id(cls, id, session = None):
+        if session is None:
+            session = DBSession
+        return session.query(cls).get(id)
